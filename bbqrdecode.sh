83e4f9258b40b81f95b2d11a183d29438ce328ce
Twijg <63592409+Twijg76@users.noreply.github.com>
2021-06-13 15:27:19 +0000

    decode added

---
+++ bbqrdecode.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Decode from camera or file (-r), automatically opens the index.* file with xdg-open
+DIR=$(pwd)
+mkdir -p ~/.cache/bbqr
+cd ~/.cache/bbqr || exit
+
+
+case "$1" in 
+	-r)
+		zbarimg -q --oneshot -Sbinary "$DIR/$2" > $$.tar.gz 2> /dev/null
+		;;
+	*)
+		zbarcam -q --oneshot -Sbinary > $$.tar.gz 2> /dev/null
+		echo "Scanning done"
+		;;
+esac
+
+mkdir -p "$$"
+tar -xzf "$$.tar.gz" -C "$$" --strip-components=1
+rm "$$.tar.gz"
+cd $$ || exit
+xdg-open index.* 2> /dev/null || echo "Can't open file, you can find all received files in ~/.cache/bbqr/$$/"
+echo "$?"
+
+[ "$?" != 0 ] && echo "Can't open file, you can find all received files in ~/.cache/bbqr/$$/"
