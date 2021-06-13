4930b284a43971582c65688db73d23b1660188bb
Twijg <63592409+Twijg76@users.noreply.github.com>
2021-06-13 20:32:55 +0000

    removed strip-components

--- bbqrdecode.sh
+++ bbqrdecode.sh
@@ -16,7 +16,7 @@ case "$1" in
 esac
 
 mkdir -p "$$"
-tar -xzf "$$.tar.gz" -C "$$" --strip-components=1
+tar -xzf "$$.tar.gz" -C "$$"
 rm "$$.tar.gz"
 cd $$ || exit
 xdg-open index.* 2> /dev/null || echo "Can't open file, you can find all received files in ~/.cache/bbqr/$$/"
